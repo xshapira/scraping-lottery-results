1daf1cc3352ee7618f01370be91030f7eaf0091f
Max Shapira <maxshapira9@gmail.com>
2022-12-29 10:50:22 +0200

    add view to check lottery results

--- lotto_results/views.py
+++ lotto_results/views.py
@@ -1,5 +1,8 @@
+import json
+
 import requests
 from bs4 import BeautifulSoup
+from django.http import HttpRequest, HTTPResponse
 from django.views import View
 
 
@@ -28,4 +31,36 @@ def scrape_lotto_results(url: str) -> list[dict[str, str]]:
 
 
 class ReviewLotteryResults(View):
-    pass
+    def get(self, request: HttpRequest, number: int) -> HTTPResponse:
+        """
+        Checks if the number is between 2500 and 3540, returning an
+        error message if it isn't. If the number is valid, it scrapes data
+        from the pais website using BeautifulSoup4.
+
+        We then checks whether any of the numbers in `lotto_results` are
+        in our list of numbers (numbers). If there's a match,
+        we return True for "is_winner". Otherwise we return False.
+
+        :param request: HttpRequest: Get the request data from the client
+        :param number: int: Specify the number of the lottery
+        :return: A httpresponse object, which is a wrapper around the
+        response that django sends back to the user
+        """
+
+        if not 2500 <= number <= 3540:
+            return HTTPResponse(
+                "Invalid number. Number must be between 2500 and 3540.", status=400
+            )
+
+        url = f"https://pais.co.il/lotto/currentlotto.aspx?lotteryId={number}"
+
+        # scrape the data from the URL
+        lotto_results = scrape_lotto_results(url)
+
+        is_winner = any(number in lotto_results["numbers"] for _ in lotto_results)
+
+        result = {"is_winner": is_winner}
+        return HTTPResponse(
+            json.dumps(result),
+            content_type="application/json",
+        )

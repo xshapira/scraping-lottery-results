b0331489cf60d98ab9f3a0ccbc0c8133f50c8105
Max Shapira <maxshapira9@gmail.com>
2022-12-31 17:35:14 +0200

    change URL for reviewing lottery results to root path

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from lotto_results.views import ReviewLotteryResults
 urlpatterns = [
     # path("admin/", admin.site.urls),
     path(
-        "review-lottery-results/",
+        "",
         ReviewLotteryResults.as_view(),
         name="review-lottery-results",
     ),
